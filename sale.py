cfc30339cd3b91f831cc1667e3a3e4d7fec361ff
resteve <resteve@zikzakmedia.com>
2015-08-28 12:18:19 +0200

    New API for on_change

--- sale.py
+++ sale.py
@@ -1,7 +1,8 @@
 # This file is part of sale_description module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
+from trytond.model import fields
 
 __all__ = ['SaleLine']
 __metaclass__ = PoolMeta
@@ -10,9 +11,9 @@ __metaclass__ = PoolMeta
 class SaleLine:
     __name__ = 'sale.line'
 
+    @fields.depends('product')
     def on_change_product(self):
-        changes = super(SaleLine, self).on_change_product()
+        super(SaleLine, self).on_change_product()
 
         if self.product and self.product.description:
-            changes['description'] = self.product.description
-        return changes
+            self.description = self.product.description
